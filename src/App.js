d5df85e3f5d15d24940c42f74cabdc8a24db2990
tvivlad <tvi-vlad@mail.ru>
2024-05-12 18:58:11 +0500

    Revert created

--- src/App.js
+++ src/App.js
@@ -47,8 +47,6 @@ function App() {
           Ваш коментарий: <span data-testid='value-elem'>{value}</span>
         </h4>
         <button>Добавить</button>
-
-        <h2>Новый текст</h2>
       </div>
     </div>
   )
